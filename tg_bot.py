c2a44af4df029e4bc1f52db1ea2b9c632b10fcd5
kaser137 <murilon375@gmail.com>
2023-12-26 19:25:24 +0200

    add buttons

--- tg_bot.py
+++ tg_bot.py
@@ -1,9 +1,10 @@
 import logging
 import os
 
-from telegram import Update, ForceReply
+import telegram
+from telegram import Update, ChatAction
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-
+from functools import wraps
 from dotenv import load_dotenv
 
 logging.basicConfig(
@@ -13,11 +14,33 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def send_action(action):
+    def decorator(func):
+        @wraps(func)
+        def command_func(update, context, *args, **kwargs):
+            context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=action)
+            return func(update, context, *args, **kwargs)
+
+        return command_func
+
+    return decorator
+
+
+send_typing_action = send_action(ChatAction.TYPING)
+
+
+@send_typing_action
 def start(update: Update, context: CallbackContext) -> None:
-    user = update.effective_user
-    update.message.reply_text('Здравствуйте')
+    custom_keyboard = [['Новый вопрос', 'Сдаться'],
+                       ['Мой счёт']]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    chat_id = update.message.chat_id
+    update.message.bot.send_message(chat_id=chat_id,
+                                    text='Здравствуйте',
+                                    reply_markup=reply_markup)
 
 
+@send_typing_action
 def echo(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
 
@@ -33,6 +56,13 @@ def main() -> None:
     updater.start_polling()
     updater.idle()
 
+
+# custom_keyboard = [['top-left', 'top-right'],
+#                    ['bottom-left', 'bottom-right']]
+# reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+# bot.send_message(chat_id=chat_id,
+#                  text="Custom Keyboard Test",
+#                  reply_markup=reply_markup)
 
 if __name__ == '__main__':
     main()
