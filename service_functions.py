b3a3ca81ee9195c1c8faeaa7e284c7fd1254c530
kaser137 <murilon375@gmail.com>
2024-01-14 16:51:37 +0200

    faster close work with file

--- service_functions.py
+++ service_functions.py
@@ -12,16 +12,16 @@ def choosing_quest():
     quiz = []
     with open(f'quiz-questions/{file_for_open}', encoding='KOI8-R') as quiz_file:
         components = quiz_file.read().split('\n\n')
-        i = 0
-        while i < len(components) - 1:
-            if components[i].strip().startswith('Вопрос'):
-                quiz.append({
-                    'question': components[i].strip(),
-                    'answer': components[i + 1].strip()
-                })
-                i += 2
-            else:
-                i += 1
+    i = 0
+    while i < len(components) - 1:
+        if components[i].strip().startswith('Вопрос'):
+            quiz.append({
+                'question': components[i].strip(),
+                'answer': components[i + 1].strip()
+            })
+            i += 2
+        else:
+            i += 1
     random_pair = random.choice(quiz)
     question = random_pair['question']
     answer = random_pair['answer']
