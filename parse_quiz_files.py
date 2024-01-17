81c94fbb6849aba35df234281c5007a1995a1e28
kaser137 <murilon375@gmail.com>
2024-01-17 19:48:36 +0200

    divide func and parsing quiz data

---
+++ parse_quiz_files.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+quiz_files_path = os.getenv('QUIZ_FILES_PATH', 'quiz-questions')
+files_list = sorted(os.listdir(quiz_files_path))
+quiz = []
+for file in files_list:
+    with open(f'{quiz_files_path}/{file}', encoding='KOI8-R') as quiz_file:
+        components = quiz_file.read().split('\n\n')
+        i = 0
+    while i < len(components) - 1:
+        if components[i].strip().startswith('Вопрос'):
+            quiz.append({
+                'question': components[i].strip(),
+                'answer': components[i + 1].strip()
+            })
+            i += 2
+        else:
+            i += 1
+QUIZ = quiz
--- receive_quest.py
+++ receive_quest.py
@@ -1,29 +1,13 @@
-import os
 import random
-from dotenv import load_dotenv
-
-
-load_dotenv()
-quiz_files_path = os.getenv('QUIZ_FILES_PATH', 'quiz-questions')
-files_list = sorted(os.listdir(quiz_files_path))
-quiz = []
-for file in files_list:
-    with open(f'{quiz_files_path}/{file}', encoding='KOI8-R') as quiz_file:
-        components = quiz_file.read().split('\n\n')
-        i = 0
-    while i < len(components) - 1:
-        if components[i].strip().startswith('Вопрос'):
-            quiz.append({
-                'question': components[i].strip(),
-                'answer': components[i + 1].strip()
-            })
-            i += 2
-        else:
-            i += 1
+from parse_quiz_files import QUIZ
 
 
 def choose_quest():
-    random_pair = random.choice(quiz)
+    random_pair = random.choice(QUIZ)
     question = random_pair['question']
     answer = random_pair['answer']
     return question, answer
+
+
+if __name__ == '__main__':
+    choose_quest()
